69ceb6731911225d44843e2d4684c0736f3cbc48
Safi <mdsafi729@gmail.com>
2023-05-07 20:08:10 +0530

    [shamsuddin]Fix: Adding UC2 file

---
+++ UC2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Func<string, bool> lastNameValidator = (lastName) =>
+            !string.IsNullOrEmpty(lastName) && char.IsUpper(lastName[0]) && lastName.Length >= 3;
+
+        Console.Write("Enter last name: ");
+        string lastName = Console.ReadLine();
+
+        if (lastNameValidator(lastName))
+        {
+            Console.WriteLine("Valid last name.");
+        }
+        else
+        {
+            Console.WriteLine("Invalid last name.");
+        }
+    }
+}
