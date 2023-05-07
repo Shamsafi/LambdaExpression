09b3295ae3580f483dc8cea4256aa26e7e919254
Safi <mdsafi729@gmail.com>
2023-05-07 20:08:45 +0530

    [shamsuddin]Fix: Adding UC3 file

---
+++ UC3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Func<string, bool> emailValidator = (email) =>
+            Regex.IsMatch(email, @"^[a-zA-Z0-9._%+-]+@(abc)+\.(bl)+\.(co)(\.(xyz|in))?");  //EX:-sam.smith@abc.bl.co.in
+
+        Console.Write("Enter email address: ");
+        string email = Console.ReadLine();
+
+        if (emailValidator(email))
+        {
+            Console.WriteLine("Valid email address.");
+        }
+        else
+        {
+            Console.WriteLine("Invalid email address.");
+        }
+    }
+}
