4e4fdeaf595c89a682b0af4b58c8c7c1f28fa5cf
Safi <mdsafi729@gmail.com>
2023-05-07 20:07:23 +0530

    [shamsuddin]Fix: Adding UC1 file

---
+++ UC1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Func<string, bool> firstNameValidator = (firstName) =>
+            !string.IsNullOrEmpty(firstName) && char.IsUpper(firstName[0]) && firstName.Length >= 3;
+
+        Console.Write("Enter first name: ");
+        string firstName = Console.ReadLine();
+
+        if (firstNameValidator(firstName))
+        {
+            Console.WriteLine("Valid first name.");
+        }
+        else
+        {
+            Console.WriteLine("Invalid first name.");
+        }
+    }
+}
