a31fab244d69923c92c7a32a1bc70b4b73ed3b2c
Safi <mdsafi729@gmail.com>
2023-05-07 20:09:24 +0530

    [shamsuddin]Fix: Adding UC4 file

---
+++ UC4.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter your mobile number: ");
+        string mobile = Console.ReadLine();
+
+        Func<string, bool> isValidMobile = (number) =>
+        {
+            if (string.IsNullOrEmpty(number) || number.Length != 13)
+            {
+                return false;
+            }
+            if (!number.StartsWith("91 "))
+            {
+                return false;
+            }
+            if (!long.TryParse(number.Substring(3), out long result))
+            {
+                return false;
+            }
+            return true;
+        };
+
+        if (isValidMobile(mobile))
+        {
+            Console.WriteLine("Valid mobile number.");
+        }
+        else
+        {
+            Console.WriteLine("Invalid mobile number.");
+        }
+    }
+}
