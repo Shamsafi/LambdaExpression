6d8e9ba93378ee3cff3a200161923549dec8e782
Safi <mdsafi729@gmail.com>
2023-05-07 20:09:56 +0530

    [shamsuddin]Fix: Adding UC5 file

---
+++ UC5.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter your password: ");
+        string password = Console.ReadLine();
+
+        Func<string, bool> isValidPassword = (pass) =>
+        {
+            if (string.IsNullOrEmpty(pass) || pass.Length < 8)
+            {
+                return false;
+            }
+            if (!pass.Any(char.IsUpper))
+            {
+                return false;
+            }
+            if (!pass.Any(char.IsLower))
+            {
+                return false;
+            }
+            if (!pass.Any(char.IsDigit))
+            {
+                return false;
+            }
+            if (!pass.Any(c => !char.IsLetterOrDigit(c)))
+            {
+                return false;
+            }
+            return true;
+        };
+
+        if (isValidPassword(password))
+        {
+            Console.WriteLine("Valid password.");
+        }
+        else
+        {
+            Console.WriteLine("Invalid password.");
+        }
+    }
+}
